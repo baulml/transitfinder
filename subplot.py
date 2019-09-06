6fd80a1b487964e2db22834cd566f34a828d658e
Paul Hildebrandt <me@paulhldbrndt.com>
2019-09-06 08:19:46 +0200

    Update subplot.py

--- subplot.py
+++ subplot.py
@@ -1,11 +1,14 @@
 import sys
 import os
+import warnings
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy import units as u
 from astropy.timeseries import BoxLeastSquares
 from astropy.stats import sigma_clipped_stats
+from astropy.table import Table
 from astropy.timeseries import TimeSeries
+from astropy.time import Time, TimeDelta
 from astropy.timeseries import aggregate_downsample
 from astropy.io import registry, fits
 import tkinter as tk
@@ -17,15 +20,34 @@ root = tk.Tk()
 root.withdraw()
 filepath = filedialog.askopenfilename()
 
-keplerID = "555"
 
 
+hdulist = fits.open(filepath)
+telescope = hdulist[0].header['telescop'].lower()
+if telescope == 'tess':
+    hdu = hdulist['LIGHTCURVE']
+    print("TESS file recognized")
+    tessID = str(hdulist[1].header['TICID'])
+    print(tessID)
+    ts = TimeSeries.read(filepath, format='tess.fits')
 
-ts = TimeSeries.read(filepath,format='kepler.fits')
+elif telescope == 'kepler':
+    hdu = hdulist[1]
+    print("Kepler file recognized")
+    keplerID = str(hdulist[0].header['KEPLERID'])
+    print(keplerID)
+    ts = TimeSeries.read(filepath, format='kepler.fits')
+else:
+    raise NotImplementedError("{} is not implemented, only KEPLER or TESS are "
+                                  "supported through this reader".format(hdulist[0].header['telescop']))
+
 fig, axs = plt.subplots(2, 2)
-fig.suptitle('Kepler Id:011446443', fontsize=15)
-fig.suptitle('Kepler Id:' +keplerID, fontsize=15)
+if telescope == 'tess':
+    fig.suptitle('TESS Id:' +tessID, fontsize=15)
+if telescope == 'kepler':
+    fig.suptitle('Kepler Id:' + keplerID, fontsize=15)
 fig.set_size_inches(18.5, 10.5)
+
 
 #full data plot
 axs[0, 0].plot(ts.time.jd, ts['sap_flux'], 'k.', markersize=1, label='whole')
@@ -62,4 +84,5 @@ axs[1, 1].plot(ts_binned.time_bin_start.jd, ts_binned['sap_flux_norm'], 'r-', dr
 axs[1, 1].set_xlabel('Time (days)')
 axs[1, 1].set_ylabel('Normalized Flux')
 axs[1, 1].set_title('transit model')
+
 plt.show()
